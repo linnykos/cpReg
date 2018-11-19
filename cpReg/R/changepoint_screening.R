1f373fdf7fbfb57fa4f328ad820c554442e47842
klin1234@gmail.com <klin1234@gmail.com>
2018-11-19 10:22:48 -0500

    Commiting an untesting version

--- cpReg/R/changepoint_screening.R
+++ cpReg/R/changepoint_screening.R
@@ -1,5 +1,38 @@
 .changepoint_screening <- function(dat, fit, gamma){
+  h <- hash::hash()
+  partition <- fit$partition
+  A_list <- .unravel(partition, fit$A_list)
 
+  .enumerate_all_cp <- function(partition){
+    len <- length(partition)
+    if(len < 3) return(NA)
+    for(i in 1:(len-2)){
+      cusum <- .multidimen_cusum(A_list[(partition[i]+1):partition[i+1]],
+                                 A_list[(partition[i+1]+1):partition[i+2]])
+      val <- .l2norm(cusum)
+      if(val < gamma){
+        return(i+1)
+      }
+    }
+
+    NA
+  }
+
+  while(TRUE){
+    tmp <- .enumerate_all_cp(partition)
+    if(is.na(tmp)) break()
+    partition <- partition[-tmp]
+  }
+
+  partition
+}
+
+
+
+.construct_hash_name <- function(s,t,e){
+  stopifnot(s < t, t < e)
+
+  paste0(s, "-", t, "-", e)
 }
 
 # converts k-1 estimates of A into T estimates of A
